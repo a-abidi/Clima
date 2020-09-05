84c756402bd4d348343d176a7be3a1760c255024
a-abidi <aliabidi@icloud.com>
2020-09-05 13:13:09 +0100

    Add functions to control the text field input.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) // ends editing when return pressed
+        print(searchTextField.text!)
+        return true
+    }
+    
+    @IBAction func searchPressed(_ sender: Any) {        searchTextField.endEditing(true) // ends editing when return pressed
+        print(searchTextField.text!)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = "" // to clear the text field after return
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = ">:("
+            return false
+        }
+    }
+    
 
 }
 
